24a02b26e9d6914794153e2e4fc11bc19fd1ba71
Roberto Attanasio <r.attanasio90@gmail.com>
2026-08-05 13:43:12 +0200

    Margo Grid utilities added

---
+++ .changeset/kind-spiders-leave.md
@@ -0,0 +1,5 @@
+---
+"margo-ui": patch
+---
+
+Margo Grid utilities added
--- src/utils/cn/cn.ts
+++ src/utils/cn/cn.ts
@@ -3,10 +3,45 @@ import { extendTailwindMerge } from "tailwind-merge";
 
 import type { ClassValue } from "clsx";
 
-const twMerge = extendTailwindMerge({
+const isNumber = (value: string) => /^\d+$/.test(value);
+
+const isColumnLine = (value: string) => value === "full" || value === "content" || isNumber(value);
+
+type MargoClassGroupIds =
+  | "margo-col"
+  | "margo-col-start"
+  | "margo-col-end"
+  | "margo-col-span"
+  | "margo-row"
+  | "margo-row-start"
+  | "margo-row-span"
+  | "margo-gutter"
+  | "margo-gutter-x"
+  | "margo-gutter-y"
+  | "margo-padding";
+
+const twMerge = extendTailwindMerge<MargoClassGroupIds>({
   extend: {
     classGroups: {
       "font-size": ["text-mc"],
+      "margo-col": [{ "margo-col": [isColumnLine] }],
+      "margo-col-start": [{ "margo-col-start": [isColumnLine] }],
+      "margo-col-end": [{ "margo-col-end": [isColumnLine] }],
+      "margo-col-span": [{ "margo-col-span": [isColumnLine] }],
+      "margo-row": [{ "margo-row": [isNumber] }],
+      "margo-row-start": [{ "margo-row-start": [isNumber] }],
+      "margo-row-span": [{ "margo-row-span": [isNumber] }],
+      "margo-gutter": [{ "margo-gutter": [isNumber] }],
+      "margo-gutter-x": [{ "margo-gutter-x": [isNumber] }],
+      "margo-gutter-y": [{ "margo-gutter-y": [isNumber] }],
+      "margo-padding": [{ "margo-padding": [isNumber] }],
+    },
+    conflictingClassGroups: {
+      "margo-col": ["margo-col-start", "margo-col-end", "margo-col-span"],
+      "margo-col-span": ["margo-col-end"],
+      "margo-col-end": ["margo-col-span"],
+      "margo-row": ["margo-row-start", "margo-row-span"],
+      "margo-gutter": ["margo-gutter-x", "margo-gutter-y"],
     },
   },
 });
