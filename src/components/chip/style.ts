25b9b26e7420e29393870a196f03c74f2aea5be7
Roberto Attanasio <r.attanasio90@gmail.com>
2026-08-13 18:19:40 +0200

    Chip style refactor

--- src/components/chip/style.ts
+++ src/components/chip/style.ts
@@ -1,4 +1,5 @@
-export const chipBaseClassName =
-  "bg-on-main/8 margo-text-box-trim flex items-center justify-center px-3 py-0.5 whitespace-nowrap lowercase flex-none leading-none rounded-md text-mc shadow-chip";
+export const chipBaseClassName = `bg-on-main/8 margo-text-box-trim flex items-center justify-center px-3 py-0.5
+whitespace-nowrap lowercase flex-none leading-none rounded-md text-mc shadow-chip
+focus-visible:outline focus-visible:outline-offset-1 focus-visible:outline-on-main`;
 
 export const chipActiveClassName = "bg-on-main text-main";
